aeb6f6f3b0df1cce2668e643eb04e296a0117593
Nightfury874 <jibaba874@gmail.com>
2023-09-19 13:38:55 +0530

    Application can download image and check for duplicates

--- src/main/java/com/example/imagedownload/ImageController.java
+++ src/main/java/com/example/imagedownload/ImageController.java
@@ -14,6 +14,9 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.http.util.EntityUtils;
 import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -39,35 +42,59 @@ public class ImageController {
                 // Read the image data
                 byte[] imageBytes = EntityUtils.toByteArray(entity);
 
-                // Save the image (we'll cover this in the next step)
+                // Save the image
                 saveImage(imageBytes, url);
+
+                // Set success message
+                model.addAttribute("success", true);
+            }
+        } catch (IOException e) {
+            if ("Image has already been downloaded. Please provide a new link.".equals(e.getMessage())) {
+                model.addAttribute("error", e.getMessage());
+            } else {
+                model.addAttribute("error", "Error downloading the image: " + e.getMessage());
             }
         } catch (Exception e) {
-            model.addAttribute("error", "Error downloading the image: " + e.getMessage());
-            return "index";
+            model.addAttribute("error", "Unexpected error: " + e.getMessage());
         }
 
-        // Return success message or redirect as needed
         return "index";
     }
+
+
     private void saveImage(byte[] imageBytes, String url) throws IOException {
-        // Extract the image name from the URL (this is a simple approach; you might want to refine it)
-        String imageName = url.substring(url.lastIndexOf("/") + 1);
+        try {
+            // Generate the hash for the image content
+            String imageHash = generateHash(imageBytes);
 
-        // Specify the download location
-        String downloadLocation = "/Users/niharbasisth/Desktop"; // This can be externalized to application.properties
+            // Specify the download location
+            String downloadLocation = "/Users/niharbasisth/Desktop/Landing"; // This can be externalized to application.properties
 
-        // Check if the image already exists
-        File imageFile = new File(downloadLocation + File.separator + imageName);
-        if (imageFile.exists()) {
-            throw new IOException("Image has already been downloaded. Please provide a new link.");
-        }
+            // Check if an image with the same hash already exists
+            File imageFile = new File(downloadLocation + File.separator + imageHash + ".jpg"); // Using .jpg extension, adjust if needed
+            if (imageFile.exists()) {
+                throw new IOException("Image has already been downloaded. Please provide a new link.");
+            }
 
-        // Save the image
-        try (FileOutputStream fos = new FileOutputStream(imageFile)) {
-            fos.write(imageBytes);
+            // Save the image
+            try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+                fos.write(imageBytes);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new IOException("Error generating image hash: " + e.getMessage());
         }
     }
+
+    private String generateHash(byte[] imageBytes) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(imageBytes);
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+
 
 
 }
